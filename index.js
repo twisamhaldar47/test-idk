caac76a7d241b57146397a856b0ce682a9af3897
twisamhaldar47 <twisampati.haldar@techwens.in>
2024-03-15 00:07:18 +0530

    sory

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
